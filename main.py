06f4970bcf9e25ecdf58b10edda64a8262d8378a
Aleksandr Babynin <babynin.home@gmail.com>
2026-01-30 00:13:18 +1100

    Сделал апдейт скрипта согласно улучшениям

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
-
+HH_MOSCOW_AREA_ID = 1
+HH_SEARCH_PERIOD_DAYS = 30
+SJ_MOSCOW_TOWN_ID = 4
 IT_LANGUAGES = [
     'Python',
     'Java',
@@ -17,27 +19,16 @@ IT_LANGUAGES = [
 ]
 
 
-def get_vacancies_count(it_language):
-    params = {
-        'text': f'программист {it_language}',
-        'area': 1,
-        'period': 30,
-    }
-    url = 'https://api.hh.ru/vacancies'
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    return response.json()['found']
-
-
 def get_vacancies_hh(it_language):
     all_vacancies = []
     page = 0
+    found = None
     while True:
         params = {
             'text': f'программист {it_language}',
-            'area': 1,
-            'period': 30,
-            'only_with_salary': True,
+            'area': HH_MOSCOW_AREA_ID,
+            'period': HH_SEARCH_PERIOD_DAYS,
+            'only_with_salary': False,
             'page': page,
             'per_page': 100
         }
@@ -45,11 +36,13 @@ def get_vacancies_hh(it_language):
         response = requests.get(url, params=params)
         response.raise_for_status()
         response_data = response.json()
+        if found is None:
+            found = response_data['found']
         all_vacancies.extend(response_data['items'])
         if page >= response_data['pages'] - 1:
             break
         page += 1
-    return all_vacancies
+    return found, all_vacancies
 
 
 def get_vacancies_sj(secret_key, it_language):
@@ -57,8 +50,8 @@ def get_vacancies_sj(secret_key, it_language):
         'X-Api-App-Id': secret_key
     }
     params = {
-        'keyword': f'{it_language}',
-        'town': 4
+        'keyword': f'программист {it_language}',
+        'town': SJ_MOSCOW_TOWN_ID
     }
     url = 'https://api.superjob.ru/2.0/vacancies'
     response = requests.get(url, params=params, headers=headers)
@@ -67,50 +60,48 @@ def get_vacancies_sj(secret_key, it_language):
     return response_data
 
 
-def predict_rub_salary_hh(vacancies):
+def predict_salary(salary_from, salary_to):
+    if not salary_from and not salary_to:
+        return None
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_from:
+        return salary_from * 1.2
+    elif salary_to:
+        return salary_to * 0.8
+
+
+def predict_rub_salaries_hh(vacancies):
     salaries = []
     for vacancy in vacancies:
         salary = vacancy.get('salary')
-        if salary.get('currency') != 'RUR':
+        if not salary or salary.get('currency') != 'RUR':
             salaries.append(None)
             continue
         salary_from = salary.get('from')
         salary_to = salary.get('to')
-        if salary_from and salary_to:
-            salaries.append((salary_from+salary_to)/2)
-        elif salary_from:
-            salaries.append(salary_from*1.2)
-        elif salary_to:
-            salaries.append(salary_to*0.8)
+        salaries.append(predict_salary(salary_from, salary_to))
     return salaries
 
 
-def predict_rub_salary_sj(vacancy):
+def predict_rub_salaries_sj(vacancy):
     salary_from = vacancy.get('payment_from')
     salary_to = vacancy.get('payment_to')
-    if not salary_from and not salary_to:
-        return None
-    if salary_from and salary_to:
-        return (salary_from+salary_to)/2
-    elif salary_from:
-        return salary_from*1.2
-    elif salary_to:
-        return salary_to*0.8
+    return predict_salary(salary_from, salary_to)
 
 
-def get_hh_salaries(IT_LANGUAGES):
+def get_hh_salaries(it_languages):
     hh_salaries = {}
-    for it_language in IT_LANGUAGES:
-        hh_vacancies_found = get_vacancies_count(it_language)
-        if hh_vacancies_found < 100:
+    for it_language in it_languages:
+        hh_vacancies_found, vacancies = get_vacancies_hh(it_language)
+        if hh_vacancies_found < 100:  # оставляем фильтр как у тебя было
             continue
-        vacancies = get_vacancies_hh(it_language)
-        salaries = predict_rub_salary_hh(vacancies)
+        salaries = predict_rub_salaries_hh(vacancies)
         valid_salaries_hh = []
         for salary in salaries:
-            if salary is not None:
+            if salary:
                 valid_salaries_hh.append(salary)
-        average_salary_hh = (sum(valid_salaries_hh) / len(valid_salaries_hh))
+        average_salary_hh = sum(valid_salaries_hh) / len(valid_salaries_hh)
         hh_salaries[it_language] = {
             'vacancies_found': hh_vacancies_found,
             'vacancies_processed': len(valid_salaries_hh),
@@ -119,23 +110,28 @@ def get_hh_salaries(IT_LANGUAGES):
     return hh_salaries
 
 
-def get_sj_salaries(secret_key, IT_LANGUAGES):
+def get_sj_salaries(secret_key, it_languages):
     sj_salaries = {}
-    for it_language in IT_LANGUAGES:
+    for it_language in it_languages:
         all_vacancies = get_vacancies_sj(secret_key, it_language)
+        sj_vacancies_found = all_vacancies.get('total', 0)
         vacancies = all_vacancies['objects']
         language_vacancies = []
         for vacancy in vacancies:
             profession = vacancy.get('candidat', '')
             if it_language.lower() in profession.lower():
                 language_vacancies.append(vacancy)
-        sj_vacancies_found = len(language_vacancies)
         valid_salaries_sj = []
         for vacancy in language_vacancies:
-            salary = predict_rub_salary_sj(vacancy)
-            if salary is not None:
+            salary = predict_rub_salaries_sj(vacancy)
+            if salary:
                 valid_salaries_sj.append(salary)
         if not valid_salaries_sj:
+            sj_salaries[it_language] = {
+                'vacancies_found': sj_vacancies_found,
+                'vacancies_processed': 0,
+                'average_salary': 0
+            }
             continue
         average_salary_sj = sum(valid_salaries_sj)/len(valid_salaries_sj)
         sj_salaries[it_language] = {
@@ -171,9 +167,9 @@ def main():
     secret_key = os.getenv('SJ_KEY')
     hh_stats = get_hh_salaries(IT_LANGUAGES)
     sj_stats = get_sj_salaries(secret_key, IT_LANGUAGES)
-    print(make_table(hh_stats, 'HeadHunter Moskow'))
+    print(make_table(hh_stats, 'HeadHunter Moscow'))
     print()
-    print(make_table(sj_stats, 'SuperJob Moskow'))
+    print(make_table(sj_stats, 'SuperJob Moscow'))
 
 
 if __name__ == '__main__':
